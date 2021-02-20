320d3f474441a26e4b1c64935c7452e725621bb0
izlyforever <izlyforever@outlook.com>
2021-02-21 01:19:08 +0800

    update

--- debug.cpp
+++ debug.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 #define watch(x) std::cout << (#x) << " is " << (x) << std::endl
 using LL = long long;
-#include "C:/Users/dna049/cf/include/izlyforever.hpp"
+#include "include/izlyforever.hpp"
 
 int main() {
-	freopen("C:\\Users\\dna049\\cf\\in", "r", stdin);
+	// freopen("C:\\Users\\dna049\\cf\\in", "r", stdin);
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	auto start = std::clock();
