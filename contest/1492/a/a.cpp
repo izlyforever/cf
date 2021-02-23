c14c82972e630ab9c8c47b617e9d376bdd99efff
izlyforever <izlyforever@outlook.com>
2021-02-23 19:10:12 +0800

    update

---
+++ contest/1492/a/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		LL a[3], p;
+		std::cin >> p;
+		for (int i = 0; i < 3; ++i) std::cin >> a[i];
+		LL r = INT64_MAX;
+		for (int i = 0; i < 3; ++i) {
+			LL t = p % a[i];
+			r = std::min(r, (t == 0 ? 0 : a[i] - t));
+		}
+		std::cout << r << "\n";
+	}
+	return 0;
+}
---
+++ contest/1492/c/c.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int n, m;
+	std::cin >> n >> m;
+	std::string a, b;
+	std::cin >> a >> b;
+	std::vector<int> c(m), d(m);
+	for (int i = 0, j = 0; i < m; ++i) {
+		while (a[j] != b[i]) ++j;
+		c[i] = j++;
+	}
+	for (int i = m - 1, j = n - 1; i >= 0; --i) {
+		while (a[j] != b[i]) --j;
+		d[i] = j--;
+	}
+	int r = 1;
+	for (int i = 0; i + 1 < m; ++i) {
+		r = std::max(r, d[i + 1] - c[i]);
+	}
+	std::cout << r << "\n";
+	return 0;
+}
