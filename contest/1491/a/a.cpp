df2a58dc98191a6178a0a4913a91eabf1e2beafe
izlyforever <izlyforever@outlook.com>
2021-03-01 01:02:41 +0800

    update

---
+++ contest/1491/a/a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<int> a(n);
+	int r = 0;
+	for (auto &x : a) {
+		std::cin >> x;
+		r += x;
+	}
+	while (m--) {
+		int t, x;
+		std::cin >> t >> x;
+		--x;
+		if (t == 1) {
+			if (a[x] == 1) --r;
+			else ++r;
+			a[x] = 1 - a[x];
+		} else {
+			std::cout << int(x < r) << "\n";
+		}
+	}
+	return 0;
+}
