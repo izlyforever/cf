1e6cffe75e8a415e4a9932804b92375b72c1f904
izlyforever <izlyforever@outlook.com>
2021-03-15 15:19:52 +0800

    update

---
+++ contest/1500/a/a.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+const int N = 5e6 + 2;
+
+std::optional<std::tuple<int, int, int, int>> solve() {
+	int n;
+	std::cin >> n;
+	std::vector<int> a(n);
+	for (auto &x : a) std::cin >> x;
+	std::vector<std::array<int, 2>> p(N);
+	int x = -1;
+	for (int i = 0; i < n; ++i) {
+		if (p[a[i]][0] == 0) {
+			p[a[i]][0] = i + 1;
+		} else if (p[a[i]][1] == 0) {
+			p[a[i]][1] = i + 1;
+			if (x != -1) return std::make_tuple(p[a[i]][0], p[x][0], p[x][1], p[a[i]][1]);
+			x = a[i];
+		}
+	}
+	std::vector<std::array<int, 2>> r(N);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < i; ++j) {
+			int x = a[i] + a[j];
+			if (r[x][0] == r[x][1]) {
+				r[x][0] = i + 1;
+				r[x][1] = j + 1;
+			} else if (std::set{r[x][0], r[x][1], i + 1, j + 1}.size() == 4) {
+				return std::make_tuple(r[x][0], r[x][1], i + 1, j + 1);
+			}
+		}
+	}
+	return {}; // 必须要加，否则会 RE
+}
+
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	auto r = solve();
+	if (r.has_value()) {
+		std::cout << "YES\n";
+		auto [x, y, z, w] = r.value();
+		std::cout << x << ' ' << y << ' ' << z << ' ' << w << '\n';
+	} else std::cout << "NO\n";
+	return 0;
+}
---
+++ contest/1500/b/b.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+// ax + by = gcd(a,b)
+std::tuple<LL, LL, LL> exGcd(LL a, LL b) {
+	if (b == 0) return {a, 1, 0};
+	auto [d, y, x] = exGcd(b, a % b);
+	return {d, x, y - a / b * x};
+}
+
+const int N = 1e6 + 2;
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int n, m;
+	LL k;
+	std::cin >> n >> m >> k;
+	std::vector<int> a(n), b(m);
+	for (auto &x : a) std::cin >> x;
+	for (auto &x : b) std::cin >> x;
+	std::vector<int> pa(N, -1), pb(N, -1);
+	for (int i = 0; i < n; ++i) pa[a[i]] = i;
+	for (int i = 0; i < m; ++i) pb[b[i]] = i;
+	auto [d, tn, tm] = exGcd(n, m);
+	LL nm = LL(n) * m, mod = nm / d;
+	std::vector<LL> c;
+	for (int i = 0; i < N; ++i) if (pa[i] != -1 && pb[i] != -1 && (pa[i] - pb[i]) % d == 0) {
+		LL mi = (pa[i] + (pb[i] - pa[i]) / d * tn % m * n) % mod;
+		if (mi < 0) mi += mod;
+		c.emplace_back(mi);
+	}
+	auto f = [&](LL x) {
+		LL r = x, tx = x % mod;
+		for (auto val : c) if (val < tx) --r;
+		return r - x / mod * c.size();
+	};
+	LL l = k, r = k * std::max(n, m);
+	while (l <= r) {
+		LL m = (l + r) / 2;
+		if (f(m) >= k) r = m - 1;
+		else l = m + 1;
+	}
+	std::cout << l << '\n';
+	return 0;
+}
