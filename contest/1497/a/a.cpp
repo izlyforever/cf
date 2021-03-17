41787bba587a1c3cec8abafa6ee71e8f5a5881d8
izlyforever <izlyforever@outlook.com>
2021-03-18 00:52:10 +0800

    update

---
+++ contest/1497/a/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		int n;
+		std::cin >> n;
+		std::multiset<int> S;
+		for (int i = 0, x; i < n; ++i) {
+			std::cin >> x;
+			S.insert(x);
+		}
+		int now = 0;
+		while (S.count(now)) {
+			std::cout << now << ' ';
+			S.erase(S.find(now));
+			++now;
+		}
+		for (auto x : S) std::cout << x << ' ';
+		std::cout << '\n';
+	}
+	return 0;
+}
---
+++ contest/1497/c2/c2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		int n, k;
+		std::cin >> n >> k;
+		for (int i = 3; i < k; ++i) {
+			std::cout << "1 ";
+			--n;
+		}
+		int x = n & -n;
+		if (x == n) {
+			std::cout << n / 4 << ' ' << n / 4 << ' ' << n / 2 << '\n';
+		} else {
+			std::cout << x << ' ' << (n - x) / 2 << ' ' << (n - x) / 2 << '\n';
+		}
+	}
+	return 0;
+}
