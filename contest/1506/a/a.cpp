db23a1581f13e3d4123388653b512a0a62e9b6c7
izlyforever <72483314+izlyforever@users.noreply.github.com>
2021-03-29 13:40:53 +0800

    update

---
+++ contest/1506/a/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		LL n, m, x;
+		std::cin >> n >> m >> x;
+		--x;
+		LL y = x / n + (x % n) * m;
+		std::cout << ++y << '\n';
+	}
+	return 0;
+}
---
+++ contest/1506/g/g.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		std::string s;
+		std::cin >> s;
+		std::vector<int> lst(26, -1);
+		int now = -1;
+		for (auto c : s) lst[c - 'a'] = ++now;
+		std::vector<int> a, cnt(26);
+		for (int i = 0; i < s.size(); ++i) if (!cnt[s[i] - 'a']) {
+			while (!a.empty() && s[a.back()] < s[i] && lst[s[a.back()] - 'a'] > i) {
+				--cnt[s[a.back()] - 'a'];
+				a.pop_back();
+			}
+			cnt[s[i] - 'a'] = 1;
+			a.emplace_back(i);
+		}				
+		for (auto x : a) std::cout << s[x]; 
+		std::cout << '\n';
+	}
+	return 0;
+}
