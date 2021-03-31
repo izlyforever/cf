a77d9d7b558cf7b3cc57f11291a2f63249ec4ffe
izlyforever <izlyforever@outlook.com>
2021-03-31 17:59:51 +0800

    update

--- contest/1331/h/h.cpp
+++ contest/1331/h/h.cpp
@@ -6,10 +6,11 @@ int main() {
 	//freopen("in", "r", stdin);
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
-	int cas = 1;
-	std::cin >> cas;
-	while (cas--) {
-
-	}
+	int x;
+	std::cin >> x;
+	int n = x / 1000;
+	int mod = x % 1000, r = 1;
+	for (int i = n; i > 1; i -= 2) r = r * i % mod;
+	std::cout << r << '\n';
 	return 0;
 }
