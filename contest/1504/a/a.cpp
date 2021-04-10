7680ee51a04cc43f835cb4687f319ac16aa9046a
izlyforever <izlyforever@outlook.com>
2021-04-10 22:18:08 +0800

    update

---
+++ contest/1504/a/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		std::string s;
+		std::cin >> s;
+		int i = 0;
+		while (i < s.size() && s[i] == 'a') ++i;
+		if (i == s.size()) {
+			std::cout << "NO\n";
+		} else {
+			std::cout << "YES\n";
+			s.insert(s.begin() + s.size() - i, 'a');
+			std::cout << s << '\n';
+		}
+	}
+	return 0;
+}
---
+++ contest/1504/b/b.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+bool solve() {
+	int n;
+	std::string a, b;
+	std::cin >> n >> a >> b;
+	std::vector<int> c;
+	int now = 0;
+	for (auto x : a) {
+		if (x == '0') --now;
+		else ++now;
+		c.emplace_back(now);
+	}
+	bool flag = true;
+	while (n) {
+		--n;
+		if ((a[n] == b[n]) ^ flag) {
+			if (c[n] != 0) return 0;
+			flag = !flag;
+		}
+	}
+	return 1;
+}
+
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		std::cout << (solve() ? "YES\n" : "NO\n");
+	}
+	return 0;
+}
