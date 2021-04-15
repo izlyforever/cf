38880e8dc83a687ca35306f722ba3cc106a394b0
izlyforever <72483314+izlyforever@users.noreply.github.com>
2021-04-16 00:53:29 +0800

    update

--- gym/103048/h/h.cpp
+++ gym/103048/h/h.cpp
@@ -1,3 +1,9 @@
+// 下面代码不能被向量化，因此加下面优化也没啥用
+// #ifdef ONLINE_JUDGE
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,avx,avx2,popcnt,tune=native")
+// #endif
+
 #include <bits/stdc++.h>
 #define watch(x) std::cout << (#x) << " is " << (x) << std::endl
 using LL = long long;
@@ -10,16 +16,16 @@ int main() {
 	std::vector<int> x(n), y(n);
 	for (int i = 0; i < n; ++i) std::cin >> x[i] >> y[i];
 	LL ans = 0;
-	std::function<void(int, int)> diviedConqer = [&](int l, int r) {
-		if (r - l == 1) {
-			ans = std::max(ans, LL(x[l]) * y[l]);
-		} else {
-			int m = (l + r) / 2;
-			diviedConqer(l, m);
-			diviedConqer(m, r);			
-			// 分四种情况，咋变成 O(n) 呢
-			
+	for (int i = 0; i < n; ++i) {
+		int mx = INT_MAX, my = INT_MAX;
+		for (int j = i; j < n; ++j) {
+			mx = std::min(mx, x[j]);
+			my = std::min(my, y[j]);
+			// LL t = LL(mx) * my;
+			// if (t * (n - i) < ans) break;
+			ans = std::max(ans, LL(j - i + 1) * mx * my);
 		}
-	};
+	}
+	std::cout << ans << '\n';
 	return 0;
 }
