92a1d2d3e919833c4bd3ebc308b787250594fe2d
izlyforever <izlyforever@outlook.com>
2021-04-17 02:33:27 +0800

    update

---
+++ contest/1509/a/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int cas = 1;
+	std::cin >> cas;
+	while (cas--) {
+		int n;
+		std::cin >> n;
+		std::vector<int> a[2];
+		for (int i = 0, x; i < n; ++i) {
+			std::cin >> x;
+			a[x & 1].emplace_back(x);
+		}
+		for (auto x : a[0]) std::cout << x << ' ';
+		for (auto x : a[1]) std::cout << x << ' ';
+		std::cout << '\n';
+	}
+	return 0;
+}
