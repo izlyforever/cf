ed34c5ff4cc4930b80a9972c13ba6cbbcf983bc3
izlyforever <72483314+izlyforever@users.noreply.github.com>
2021-05-06 18:33:42 +0800

    update

--- contest/0/a/a.cpp
+++ contest/0/a/a.cpp
@@ -2,23 +2,25 @@
 #define watch(x) std::cout << (#x) << " is " << (x) << std::endl
 using LL = long long;
 
-class PstDSU {
-public:
-	int n;
+// fa 里面存编号会导致更新时儿子节点无法追踪到新的父节点。
+class PstSegTree {
 	struct Node {
 		int l, r, fa, dep;
 	};
-	int newNode () {
+	int newNode() {
 		int sz = tree.size();
 		tree.emplace_back(Node());
 		return sz;
 	}
 	std::vector<Node> tree;
+	int n;
+public:
 	std::vector<int> root;
-	PstDSU(int _n) : n(_n) {
+	PstSegTree(int _n) : n(_n) {
 		std::function<void(int, int, int)> build = [&](int l, int r, int p) {
 			if (r - l == 1) {
 				tree[p].fa = l;
+				tree[p].dep = 0;
 			} else {
 				int m = (l + r) / 2;
 				build(l, m, tree[p].l = newNode());
@@ -28,42 +30,57 @@ public:
 		root.emplace_back(newNode());
 		build(0, n, root.back());
 	}
-	int find(int x, int p) {
-		int px = query(x, 0, n, p);
-		if (x == px) return x;
-		return find(px, p);
-	}
 	int query(int x, int l, int r, int p) {
-		if (r - l == 1) return tree[p].fa;
+		if (r - l == 1) return p;
 		int m = (l + r) / 2;
 		if (x < m) return query(x, l, m, tree[p].l);
 		return query(x, m, r, tree[p].r);
 	}
-	int queryDep(int x, int l, int r, int p) {
-		if (r - l == 1) return tree[p].dep;
-		int m = (l + r) / 2;
-		if (x < m) return query(x, l, m, tree[p].l);
-		return query(x, m, r, tree[p].r);
+	int find(int x, int p) {
+		int fx = tree[query(x, 0, n, p)].fa;
+		if (fx == x) return x;
+		return find(fx, p);
 	}
-	void update(int x, int y, int l, int r, int p, int q) {
-		tree[q] = tree[p];
+	int queryDep(int x, int p) {
+		return tree[query(x, 0, n, p)].dep;
+	}
+	void update(int x, int fx, int l, int r, int p, int q) {
 		if (r - l == 1) {
-			tree[q].fa = y;
+			tree[q].fa = fx;
+			tree[q].dep = tree[p].dep;
 		} else {
 			int m = (l + r) / 2;
-			if (x < m) update(x, y, l, m, tree[p].l, tree[q].l = newNode());
-			update(x, y, m, r, tree[p].r, tree[q].r = newNode());
+			if (x < m) tree[q].r = tree[p].r, update(x, fx, l, m, tree[p].l, tree[q].l = newNode());
+			else tree[q].l = tree[p].l, update(x, fx, m, r, tree[p].r, tree[q].r = newNode());
 		}
 	}
-	void merge(int x, int y, int p) {
-		int fx = find(x, p), fy = find(y, p);
-		if (fx != fy) {
+	void update(int x, int l, int r, int p, int q) {
+		if (r - l == 1) {
+			tree[q].fa = tree[p].fa;
+			tree[q].dep = tree[p].dep + 1;
+		} else {
+			int m = (l + r) / 2;
+			if (x < m) tree[q].r = tree[p].r, update(x, l, m, tree[p].l, tree[q].l = newNode());
+			else tree[q].l = tree[p].l, update(x, m, r, tree[p].r, tree[q].r = newNode());
+		}
+	}
+	void merge(int x, int y, int l, int r, int p) {
+		x = find(x, p), y = find(y, p);
+		if (x != y) {
+			int dx = queryDep(x, p), dy = queryDep(y, p);
+			if (dx > dy) std::swap(x, y);
 			root.emplace_back(newNode());
-			if (tree[fx].dep > tree[fy].dep) {
-				update(fy, fx, 0, n, p, root.back());
+			if (tree[x].dep != tree[y].dep) {
+				update(x, y, 0, n, p, root.back());
 			} else {
-				update(fx, fy, 0, n, p, root.back());
-				if (tree[fx].dep == tree[fy].dep) ++tree[fy].dep;
+				// 更新高度需要重开节点，不然高度就不正确（虽然不影响结果是否正确）
+				update(y, 0, n, p, root.back());
+				p = root.back();
+				root.emplace_back(newNode());
+				update(x, y, 0, n, p, root.back());
+				p = root.back();
+				root.pop_back();
+				root.back() = p;
 			}
 		} else root.emplace_back(root.back());
 	}
@@ -74,7 +91,7 @@ int main() {
 	std::cin.tie(nullptr)->sync_with_stdio(false);
 	int n, q;
 	std::cin >> n >> q;
-	PstDSU A(n);
+	PstSegTree A(n);
 	while (q--) {
 		int op;
 		std::cin >> op;
@@ -87,7 +104,7 @@ int main() {
 			std::cin >> a >> b;
 			--a; --b;
 			if (op == 1) {
-				A.merge(a, b, A.root.back());
+				A.merge(a, b, 0, n, A.root.back());
 			} else {
 				int fa = A.find(a, A.root.back());
 				int fb = A.find(b, A.root.back());
