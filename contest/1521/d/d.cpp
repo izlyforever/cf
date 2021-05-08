f07445c00531149da6474ee5ea69a2066ff4d500
izlyforever <72483314+izlyforever@users.noreply.github.com>
2021-05-08 12:09:33 +0800

    update

--- contest/1521/d/d.cpp
+++ contest/1521/d/d.cpp
@@ -2,57 +2,72 @@
 #define watch(x) std::cout << (#x) << " is " << (x) << std::endl
 using LL = long long;
 
-void solve() {
-	int n;
-	std::cin >> n;
-	std::vector<std::vector<int>> e(n + 1);
-	for (int i = 2; i < n; ++i) {
-		int u, v;
-		std::cin >> u >> v;
-		e[u].emplace_back(v);
-		e[v].emplace_back(u);
-	}
-	std::vector<std::tuple<int, int, int, int>> ans;
-	auto f = [&](int x) {
-		std::vector<int> sz(n + 1), son(n + 1), a(n + 1);
-		std::function<void(int, int)> pdfs = [&](int u, int fa) {
-			++sz[u];
-			for (auto v : e[u]) if (v != fa) {
-				pdfs(v, u);
-				sz[u] += sz[v];
-				if (sz[son[u]] < sz[v]) son[u] = v;
-			}
-		};
-		std::function<void(int, int)> dfs = [&](int u, int fa) {
-			if (son[u] == 0) {
-				a[u] = u;
-				return;
-			}
-			dfs(son[u], u);
-			a[u] = a[son[u]];
-			for (auto v : e[u]) if (v != fa && v != son[u]) {
-				dfs(v, u);
-				ans.emplace_back(v, u, a[u], v);
-				a[u] = a[v];
-			}
-		};
-		pdfs(x, 0);
-		dfs(x, 0);
-	};
-	int u, v;
-	std::cin >> u >> v;
-	f(u); f(v);
-	std::cout << ans.size() << '\n';
-	for (auto [x1, y1, x2, y2] : ans) std::cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << '\n';
-}
-
 int main() {
-	//freopen("in", "r", stdin);
+	// freopen("in", "r", stdin);
 	std::cin.tie(nullptr)->sync_with_stdio(false);
 	int cas = 1;
 	std::cin >> cas;
 	while (cas--) {
-		solve();
+		int n;
+		std::cin >> n;
+		std::vector<std::set<int>> g(n + 1);
+		for (int i = 1; i < n; ++i) {
+			int u, v;
+			std::cin >> u >> v;
+			g[u].insert(v);
+			g[v].insert(u);
+		}
+		std::vector<int> son(n + 1);
+		for (int i = 1; i <= n; ++i) son[i] = g[i].size() - 1;
+		++son[1];
+		std::vector<std::set<std::pair<int, int>>> e(n + 1);
+		for (int i = 1; i <= n; ++i) {
+			for (auto x : g[i]) e[i].insert({-son[x], x});
+		}
+		std::vector<std::pair<int, int>> cut, add;
+		std::function<void(int, int)> dfs = [&](int u, int fa) {
+			for (auto [_, v] : e[u]) if (v != fa) dfs(v, u);
+			if (son[u] >= 2) {
+				if (g[u].count(fa)) {
+					g[u].erase(fa);
+					g[fa].erase(u);
+					cut.emplace_back(u, fa);
+					--son[fa];
+				}
+			}
+			for (auto [_, v] : e[u]) if (v != fa && son[u] > 2) {
+				if (g[u].count(v)) {
+					g[u].erase(v);
+					g[v].erase(u);
+					cut.emplace_back(u, v);
+					--son[u];
+				}
+			}
+		};
+		dfs(1, 0);
+		std::vector<int> vis(n + 1);
+		std::vector<std::pair<int, int>> p;
+		std::vector<int> tmp;
+		std::function<void(int, int)> find = [&](int u, int fa) {
+			vis[u] = 1;
+			if (g[u].size() <= 1) tmp.emplace_back(u);
+			for (auto v : g[u]) if (v != fa) {
+				find(v, u);
+			}
+		};
+		for (int i = 1; i <= n; ++i) if (!vis[i]) {
+			tmp.clear();
+			find(i, 0);
+			p.push_back({tmp[0], tmp.back()});
+		}
+		for (int i = 1; i < p.size(); ++i) {
+			add.emplace_back(p[i - 1].second, p[i].first);
+		}
+		int sz = cut.size();
+		std::cout << sz << '\n';
+		for (int i = 0; i < sz; ++i) {
+			std::cout << cut[i].first << ' ' << cut[i].second << ' ' << add[i].first << ' ' << add[i].second << '\n';
+		}
 	}
 	return 0;
 }
--- contest/1521/e/e.cpp
+++ contest/1521/e/e.cpp
@@ -7,8 +7,64 @@ int main() {
 	std::cin.tie(nullptr)->sync_with_stdio(false);
 	int cas = 1;
 	std::cin >> cas;
+	auto f = [](int n) {
+		return (n + 1) / 2 * n;
+	};
+	auto g = [](int n) {
+		return n * n - (n / 2) * (n / 2);
+	};
 	while (cas--) {
-
+		int m, k;
+		std::cin >> m >> k;
+		std::vector<std::pair<int, int>> a(k);
+		for (int i = 0, x; i < k; ++i) {
+			std::cin >> x;
+			a[i] = {x, i + 1};
+		}
+		std::sort(a.begin(), a.end(), std::greater<>());
+		int l = 0, r = 1e4, mx = a[0].first;
+		while (l <= r) {
+			int mid = (l + r) / 2;
+			if (f(mid) >= mx && g(mid) >= m) r = mid - 1;
+			else l = mid + 1;
+		}
+		int n = l;
+		std::vector<std::vector<int>> b(n, std::vector<int>(n));
+		int now = 0;
+		// 这三个循环顺序不能变！
+		for (int i = 1; i < n; i += 2) {
+			for (int j = 0; j < n; j += 2) {
+				while (now < a.size() && a[now].first == 0) ++now;
+				if (now == a.size()) break;
+				b[i][j] = a[now].second;
+				--a[now].first;
+			}
+			if (now == a.size()) break;
+		}
+		for (int i = 0; i < n; i += 2) {
+			for (int j = 0; j < n; j += 2) {
+				while (now < a.size() && a[now].first == 0) ++now;
+				if (now == a.size()) break;
+				b[i][j] = a[now].second;
+				--a[now].first;
+			}
+			if (now == a.size()) break;
+		}
+		for (int i = 0; i < n; i += 2) {
+			for (int j = 1; j < n; j += 2) {
+				while (now < a.size() && a[now].first == 0) ++now;
+				if (now == a.size()) break;
+				b[i][j] = a[now].second;
+				--a[now].first;
+			}
+			if (now == a.size()) break;
+		}
+		std::cout << n << '\n';
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				std::cout << b[i][j] << " \n"[j == n - 1];
+			}
+		}
 	}
 	return 0;
 }
