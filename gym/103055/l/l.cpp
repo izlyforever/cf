d11cd538e24c5f89e4bfe7fe9bf71a1dbc961a36
izlyforever <72483314+izlyforever@users.noreply.github.com>
2021-05-10 00:14:49 +0800

    update

---
+++ gym/103055/l/l.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int n;
+	std::cin >> n;
+	std::string s;
+	std::cin >> s;
+	for (int i = 1; i < n; ++i) if (s[i] == s[0]) {
+		std::cout << "Wrong Answer\n";
+		return 0;
+	}
+	std::cout << "Correct\n";
+	return 0;
+}
---
+++ gym/103055/m/m.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int n;
+	std::cin >> n;
+	std::cout << "0.0000\n";
+	return 0;
+}
