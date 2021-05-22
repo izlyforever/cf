1ceb899963ca3bce6b1ab2d62373c7cde9177e00
izlyforever <72483314+izlyforever@users.noreply.github.com>
2021-05-22 17:54:23 +0800

    update

---
+++ gym/103091/h/h.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int n;
+	std::cin >> n;
+	std::set<int> S;
+	for (int i = 0, x; i < n; ++i) {
+		std::cin >> x;
+		S.insert(x);
+	}
+	std::vector<int> a(n);
+	for (auto &x : a) std::cin >> x;
+	std::sort(a.begin(), a.end(), std::greater<>());
+	int ans = 0;
+	for (auto x : a) {
+		auto it = S.lower_bound(x);
+		if (it != S.end()) {
+			S.erase(it);
+			++ans;
+		}
+	}
+	std::cout << ans << '\n';
+	return 0;
+}
---
+++ gym/103091/n/n.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+using LL = long long;
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int n;
+	std::cin >> n;
+	std::vector<int> a(n);
+	for (auto &x : a) {
+		std::cin >> x;
+		x &= 1;
+	}
+	int ans = n;
+	for (int t = 0; t < 2; ++t) {
+		int cur = 0;
+		for (int op = 0; op < 2; ++op) {
+			for (int i = op; i < n; i += 2) {
+				if ((a[i] == op) ^ t) ++cur;
+			}
+		}
+		ans = std::min(ans, cur);
+	}
+	std::cout << ans << '\n';
+	return 0;
+}
