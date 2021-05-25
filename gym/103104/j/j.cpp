e7878ce40f2a426c4cbddbbf9e2a8ae0282c15fa
izlyforever <izlyforever@outlook.com>
2021-05-26 01:19:40 +0800

    update

--- gym/103104/j/j.cpp
+++ gym/103104/j/j.cpp
@@ -2,6 +2,24 @@
 #define watch(x) std::cout << (#x) << " is " << (x) << std::endl
 using LL = long long;
 
+// 抄一下 cxy004 的做法（用复数在处理几乎问题也太帅了吧）：
+LL divAbs(LL x, LL y) {
+	LL d = x / y, r = x - d * y;
+	// 相当于按照余数绝对值最小来除
+	if (2 * r >= y) ++d;
+	else if (2 * r <= -y) --d; // 这里是注意到负数的除法是向 0 取余
+	return d;
+}
+using CL = std::complex<LL>;
+CL divCAbs(CL x, CL y) { // 这就是标准的两个复数除法
+	x *= std::conj(y);
+	LL normY = std::norm(y);
+	return {divAbs(x.real(), normY), divAbs(x.imag(), normY)};
+}
+CL gcd(CL x, CL y) {
+	return std::norm(y) == 0 ? x : gcd(y, x - divCAbs(x, y) * y);
+}
+
 int main() {
 	//freopen("in", "r", stdin);
 	std::cin.tie(nullptr)->sync_with_stdio(false);
@@ -10,57 +28,11 @@ int main() {
 	while (cas--) {
 		LL x1, y1, x2, y2, x3, y3;
 		std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-		x1 -= x3; y1 -= y3;
-		x2 -= x3; y2 -= y3;
-		LL tmp = std::gcd(std::gcd(x1, y1), std::gcd(x2, y2));
-		x1 /= tmp, x2 /= tmp, y1 /= tmp, y2 /= tmp;
-		x3 = x1 - x2; y3 = y1 - y2;
-		LL d1 = x1 * x1 + y1 * y1;
-		LL d2 = x2 * x2 + y2 * y2;
-		LL d3 = x3 * x3 + y3 * y3;
-		LL d = std::gcd(d1, std::gcd(d2, d3));
-		// 根据定理可知，d 一定可以写成两个平方因子之和!
-		if (d != 1) {
-			for (LL i = 1; i * i <= d; ++i) {
-				LL tmp = d - i * i;
-				LL j = std::sqrt(tmp + 0.2);
-				if (j * j == tmp && ((i * x1 + j * y1) % d == 0 || (j * x1 + i * y1) % d == 0)) {
-					if ((i * x1 + j * y1) % d) std::swap(i, j);
-					std::tie(x1, y1) = std::pair(i * x1 + j * y1, i * y1 - j * x1);
-					std::tie(x2, y2) = std::pair(i * x2 + j * y2, i * y2 - j * x2);
-					x1 /= d, y1 /= d;
-					x2 /= d, y2 /= d;
-					break;
-				}
-			}
-		}
-		const LL N = 1e9;
-		x3 = y3 = 0;
-		LL mx = std::max({x1, x2, x3});
-		if (mx > N) {
-			x1 -= mx - N;
-			x2 -= mx - N;
-			x3 -= mx - N;
-		}
-		mx = std::min({x1, x2, x3});
-		if (mx < -N) {
-			x1 += -N - mx;
-			x2 += -N - mx;
-			x3 += -N - mx;
-		}
-		LL my = std::max({y1, y2, y3});
-		if (my > N) {
-			y1 -= my - N;
-			y2 -= my - N;
-			y3 -= my - N;
-		}
-		my = std::min({y1, y2, y3});
-		if (my < -N) {
-			y1 += -N - my;
-			y2 += -N - my;
-			y3 += -N - my;
-		}
-		std::cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << ' ' << x3 << ' ' << y3 << '\n';
+		std::complex<LL> x(x2 - x1, y2 - y1), y(x3 - x1, y3 - y1);
+		auto d = gcd(x, y);
+		x = divCAbs(x, d);
+		y = divCAbs(y, d);
+		std::cout << "0 0 " << x.real() << ' ' << x.imag() << ' ' << y.real() << ' ' << y.imag() << '\n';
 	}
 	return 0;
 }
