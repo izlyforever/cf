cd296053ef249bb4b75e190b7a9b2d9f45cbcfaa
izlyforever <izlyforever@outlook.com>
2021-07-10 18:36:37 +0800

    update

--- update.py
+++ update.py
@@ -12,17 +12,21 @@ else:
 	os.system('git commit -m "' + ' '.join(sys.argv[1:]) + '"')
 os.system('git push origin master')
 
-
+# make local the same with remote
 # git fetch --all
 # git reset --hard origin/master
 
+# create new branch and upload
 # git checkout -b [branch name]
 # git add .
 # git commit -m "update"
 # git push origin [branch name]
 
+# switch branch
 # git checkout [branch name]
 
+
+# clear git history
 # rm -rf .git 
 # git init
 # git add -A 
