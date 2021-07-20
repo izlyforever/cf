379d41c37d7d81abc5ddce4efe3c7c6890bdec5b
izlyforever <izlyforever@outlook.com>
2021-07-20 21:23:09 +0800

    update

---
+++ contest/1550/b/b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define clog(x) std::clog << (#x) << " is " << (x) << '\n';
+#define cerr(x) std::cerr << (#x) << " is " << (x) << '\n';
+using LL = long long;
+
+// 根据情况换成 bool
+int solve() {
+    int n, a, b;
+    std::string s;
+    std::cin >> n >> a >> b >> s;
+    if (b >= 0) return (a + b) * n;
+    int x = 1;
+    for (int i = 1; i < n; ++i) if (s[i] != s[i - 1]) ++x;
+    return a * n + b * (x / 2 + 1);
+}
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int cas = 1;
+	std::cin >> cas; // 根据情况注释掉
+	while (cas--) {
+		std::cout << solve() << '\n';
+	}
+	return 0;
+}
---
+++ contest/1550/c/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define clog(x) std::clog << (#x) << " is " << (x) << '\n';
+#define cerr(x) std::cerr << (#x) << " is " << (x) << '\n';
+using LL = long long;
+
+// 根据情况换成 bool
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n), b(n);
+    for (auto &x : a) std::cin >> x;
+    int ans = 2 * n - 1;
+    for (int i = 1; i < n - 1; ++i) {
+        if (1LL * (a[i] - a[i - 1]) * (a[i] - a[i + 1]) > 0) ++ans;
+    }
+    for (int i = 2; i < n - 1; ++i) {
+        if (1LL * (a[i - 1] - a[i - 2]) * (a[i - 1] - a[i]) > 0 &&
+            1LL * (a[i] - a[i - 1]) * (a[i] - a[i + 1]) > 0 &&
+            1LL * (a[i - 1] - a[i - 2]) * (a[i - 1] - a[i + 1]) > 0 &&
+            1LL * (a[i] - a[i - 2]) * (a[i] - a[i + 1]) > 0) ++ans;
+    }
+    std::cout << ans << '\n';
+}
+
+int main() {
+	//freopen("in", "r", stdin);
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int cas = 1;
+	std::cin >> cas; // 根据情况注释掉
+	while (cas--) {
+		solve();
+	}
+	return 0;
+}
