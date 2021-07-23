5d4a629574f85f2b6fdddcf8bf3dc88a7c086a93
izlyforever <izlyforever@outlook.com>
2021-07-23 12:24:27 +0800

    update

--- contest/1553/README.md
+++ contest/1553/README.md
@@ -16,3 +16,26 @@
 
 注意到可以从后往前看，此时再注意到一次删除，本质上去掉了两个字符即可
 
+## E
+
+注意到 `cyclic shifts` 可以表达为 $p_i = (i - k) \mod n$，然后官方题解说到满足初始条件的 $k$ 必然要满足和 $a_i = p_i$ 的个数不小于 $n - 2 m$，而由于 $0 < m < \frac{n}{3}$，因此满足条件的不超过 3 个。然后平移之后看圈即可（因为要还原嘛）
+
+## F
+
+我也知道 $(x \mod y) = x - (x / y)y$, 但是哎。根据官方题解
+
+$$
+\begin{aligned}
+s_k = \sum_{1 \le i, j \le k, i > j} (a_i \bmod a_j) \\
+t_k = \sum_{1 \le i, j \le k, i < j} (a_i \bmod a_j)
+\end{aligned}
+$$
+
+因此 $s_k = s_{k - 1} + \sum_{i = 1}^{k - 1} (a_k - \lfloor \frac{a_k}{a_i} \rfloor a_i) = s_{k - 1} + a_i (k - 1) - \sum_{i = 1}^{k - 1} a_i \lfloor \frac{a_k}{a_i} \rfloor$
+
+依然不好算，因此我们对每个 $i$，看它对 $s_k$ 的贡献
+
+若 $a_k \in [d a_i, (d + 1) a_i)$, 则它的贡献就是 $-d_i$
+
+因此我们只需要在每个 $a_i$ 倍数上的点加值即可，
+
--- contest/1553/e/e.cpp
+++ contest/1553/e/e.cpp
@@ -5,7 +5,33 @@ using LL = long long;
 
 // 根据情况换成 bool
 void solve() {
-
+  int n, m;
+  std::cin >> n >> m;
+  std::vector<int> a(n), cnt(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> a[i];
+    --a[i];
+    ++cnt[(i - a[i] + n) % n];
+  }
+  std::vector<int> ans;
+  for (int k = 0; k < n; ++k) if (cnt[k] >= n - 2 * m) {
+    std::vector<int> p(n);
+    for (int i = 0; i < n; ++i) p[(i - k + n) % n] = a[i];
+    int cyc = 0;
+    std::vector<int> vis(n);
+    for (int i = 0; i < n; ++i) if (!vis[i]) {
+      int j = i;
+      while (!vis[j]) {
+        vis[j] = true;
+        j = p[j];
+      }
+      ++cyc;
+    }
+    if (n - cyc <= m) ans.emplace_back(k);
+  }
+  std::cout << ans.size();
+  for (auto x : ans) std::cout << ' ' << x;
+  std::cout << '\n';
 }
 
 int main() {
