ba3c4854680b4576456cd3a8e9d7bd041ab70024
izlyforever <izlyforever@outlook.com>
2021-08-02 03:38:43 +0800

    update

--- contest/1549/f1/f1.cpp
+++ contest/1549/f1/f1.cpp
@@ -6,16 +6,43 @@ using LL = long long;
 
 // 根据情况换成 bool
 void solve() {
-
+  int n;
+  std::cin >> n;
+  int cnt[2][2]{};
+  for (int i = 0; i < n; ++i) {
+    int x, y;
+    std::cin >> x >> y;
+    x = (x % 4 != 0);
+    y = (y % 4 != 0);
+    ++cnt[x][y];
+  }
+  auto C = [](int n, int k) -> LL {
+    if (k == 0) return 1;
+    if (k == 1) return n;
+    if (k == 2) return 1LL * n * (n - 1) / 2;
+    return 1LL * n * (n - 1) * (n - 2) / 6;
+  };
+  LL ans = 0;
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      ans += C(cnt[i][j], 3);
+      for (int k = 0; k < 2; ++k) {
+        for (int l = 0; l < 2; ++l) if (i != k || j != l) {
+          ans += C(cnt[i][j], 2) * C(cnt[k][l], 1);
+        }
+      }
+    }
+  }
+  std::cout << ans << '\n';
 }
 
 int main() {
-	//freopen("in", "r", stdin);
-	std::cin.tie(nullptr)->sync_with_stdio(false);
-	int cas = 1;
-	std::cin >> cas; // 根据情况注释掉
-	while (cas--) {
-		solve();
-	}
-	return 0;
+  //freopen("in", "r", stdin);
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int cas = 1;
+  // std::cin >> cas; // 根据情况注释掉
+  while (cas--) {
+    solve();
+  }
+  return 0;
 }
