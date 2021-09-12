9332cd11ef1b3f1091bb7165ca73d379db9515fb
izlyforever <izlyforever@outlook.com>
2021-09-13 01:29:46 +0800

    update

--- debug.cpp
+++ debug.cpp
@@ -1,11 +1,14 @@
-#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,abm,mmx,avx,avx2,popcnt,tune=native")
 #pragma comment(linker,"/STACK:10240000,10240000")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,abm,mmx,avx,avx2,popcnt,tune=native")
 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<std::pair<LL, int>, null_type, std::less<>, rb_tree_tag, tree_order_statistics_node_update>
+#define ordered_set __gnu_pbds::tree<std::pair<LL, int>,     \
+                          __gnu_pbds::null_type, std::less<>, \
+                          __gnu_pbds::rb_tree_tag,             \
+                          __gnu_pbds::tree_order_statistics_node_update>
+
 #include <bits/stdc++.h>
 #define watch(x) std::cout << (#x) << " is " << (x) << std::endl
 #define clog(x) std::clog << (#x) << " is " << (x) << '\n';
