83b220b7d0051e65c1fcc31444d5d057e966f68c
izlyforever <izlyforever@outlook.com>
2021-10-09 15:20:30 +0800

    add mimallocTest

---
+++ example/mimalloc/overrideTest.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define cerr(x) std::cerr << (#x) << " is " << (x) << '\n';
+using LL = long long;
+
+class Timer {
+  std::chrono::steady_clock::time_point start_;
+ public:
+  Timer() : start_(std::chrono::steady_clock::now()) {}
+  void show() {
+    auto elapsedTime = std::chrono::steady_clock::now() - start_;
+    std::cout << "Time used: " << (double)elapsedTime.count() / 1'000'000'000 << "s\n";
+  }
+};
+
+std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+
+LL f(int n) {
+  std::vector<uint32_t> a;
+  for (int i = 0; i < n; ++i) {
+    a.emplace_back(rnd());
+  }
+  return std::accumulate(a.begin(), a.end(), 0LL);
+}
+
+int main() {
+  int n = 1e7;
+  Timer A;
+  LL ans = f(n);
+  A.show();
+  cerr(ans);
+}
