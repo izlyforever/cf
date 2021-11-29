ba99bcf693e5d43c968dcfbce0afeb2b431ab007
izlyforever <izlyforever@outlook.com>
2021-11-29 17:03:10 +0800

    refine fastDivLambda.cpp

--- example/fastDivLambda.cpp
+++ example/fastDivLambda.cpp
@@ -1,13 +1,24 @@
 #include <bits/stdc++.h>
-#define cerr(x) std::cerr << (#x) << " is " << (x) << '\n';
+#define cerr(x) std::cerr << (#x) << " is " << (x) << '\n'
+#define NewLine std::cerr << '\n'
 
 template<typename T>
+using IntLong = std::enable_if_t<
+    std::is_same_v<int32_t, T>  ||
+    std::is_same_v<uint32_t, T> ||
+    std::is_same_v<int64_t, T>  ||
+    std::is_same_v<uint64_t, T>>;
+
+template<typename T, typename T2>
+using Twice = std::enable_if_t<sizeof(T) * 2 == sizeof(T2)>;
+
+template<typename T, typename check = IntLong<T>>
 int ctz(T x) {
-  if (sizeof(T) == 4) return __builtin_ctz(x);
+  if constexpr (sizeof(T) == 4) return __builtin_ctz(x);
   return __builtin_ctzll(x);
 }
 
-template<typename T, typename T2>
+template<typename T, typename T2, typename check = Twice<T,T2>>
 std::pair<int, T2> chooseMultiplier(T d) {
   constexpr int bitLen = __CHAR_BIT__ * sizeof(T);
   if ((d & -d) == d) return {ctz(d), T2(1)};
@@ -18,7 +29,7 @@ std::pair<int, T2> chooseMultiplier(T d) {
   return {l, (lb + 1 << l) / d};
 }
 
-template<typename T, typename T2>
+template<typename T, typename T2, typename check = Twice<T,T2>>
 std::function<T(T)> getDivFun(T d) {
   assert(d != 0);
   constexpr int bitLen = __CHAR_BIT__ * sizeof(T);
@@ -76,11 +87,12 @@ inline uint32_t div14(uint32_t n) {
 }
 
 int main() {
-  const int N = 1e8 + 2;
+  const int N = 1e7 + 2;
   // test for mod = 14
   {
     std::vector<uint32_t> a(N);
     for (auto &x : a) x = rnd();
+    const uint32_t modConst = 14;
     uint32_t mod = 14;
     for (auto x : a) {
       if (div14(x) != x / mod) {
@@ -89,18 +101,25 @@ int main() {
       }
     }
     {
-      Timer A("const default");
-      uint64_t sum = 0;
-      for (auto x : a) sum += x / mod;
-      cerr(sum);
-    }
-    {
       Timer A("mod14");
       uint64_t sum = 0;
       for (auto x : a) sum += div14(x);
       cerr(sum);
     }
+    {
+      Timer A("default");
+      uint64_t sum = 0;
+      for (auto x : a) sum += x / mod;
+      cerr(sum);
+    }
+    {
+      Timer A("const default");
+      uint64_t sum = 0;
+      for (auto x : a) sum += x / modConst;
+      cerr(sum);
+    }
   }
+  NewLine;
   // test for uint32
   {
     std::vector<uint32_t> a(N);
@@ -127,6 +146,7 @@ int main() {
       cerr(sum);
     }
   }
+  NewLine;
   // test for uint64
   {
     std::vector<uint64_t> a(N);
