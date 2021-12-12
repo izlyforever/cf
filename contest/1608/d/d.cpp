c680619f750e9e8d4034ee38fdbb57d161b95e66
izlyforever <izlyforever@outlook.com>
2021-12-12 23:35:14 +0800

    resolve 1608D

--- contest/1608/d/d.cpp
+++ contest/1608/d/d.cpp
@@ -2,7 +2,8 @@
 #define cerr(x) std::cerr << (#x) << " is " << (x) << '\n'
 using LL = long long;
 
-int powMod(int x, int n, int M) {
+const int M = 998244353;
+int powMod(int x, int n) {
   int r = 1;
   while (n) {
     if (n & 1) r = 1LL * r * x % M;
@@ -12,58 +13,43 @@ int powMod(int x, int n, int M) {
 }
 
 int solve() {
-  const int M = 998244353;
   int n;
   std::cin >> n;
-  std::vector<int> l(n), r(n);
-  std::map<char, int> mp{{'?', 0}, {'B', 1}, {'W', 2}};
-  int countl[3]{}, countr[3]{};
-  for (int i = 0; i < n; ++i) {
-    char cl, cr;
-    std::cin >> cl >> cr;
-    l[i] = mp[cl];
-    r[i] = mp[cr];
-    ++countl[l[i]];
-    ++countr[r[i]];
-  }
-  for (int _ = 0; _ < 3; ++_) {
-    for (int c = 1; c < 3; ++c) if (countl[c] == n) {
-      return countr[c] == 0;
-    }
-    for (int c = 1; c < 3; ++c) if (countr[c] == n) {
-      return countl[c] == 0;
-    }
-    for (int c = 1; c < 3; ++c) if (countl[c] == n - 1) {
-      for (int i = 0; i < n; ++i) if (l[i] != c) {
-        if (r[i] && r[i] != 3 - c) return 0;
-        r[i] = 3 - c;
-      }
-    }
-    for (int c = 1; c < 3; ++c) if (countr[c] == n - 1) {
-      for (int i = 0; i < n; ++i) if (r[i] != c) {
-        if (l[i] && l[i] != 3 - c) return 0;
-        l[i] = 3 - c;
-      }
-    }
-  }
-  std::vector<int> fac(n + 1, 1), ifac(n + 1, 1);
-  for (int i = 1; i <= n; ++i) fac[i] = 1LL * fac[i - 1] * i % M;
-  ifac[n] = powMod(fac[n], M - 2, M);
-  for (int i = n; i >= 1; --i) ifac[i - 1] = 1LL * ifac[i] * i % M;
+  int n2 = n * 2;
+  std::vector<int> fac(n2 + 1, 1), ifac(n2 + 1, 1);
+  for (int i = 1; i <= n2; ++i) fac[i] = 1LL * fac[i - 1] * i % M;
+  ifac[n2] = powMod(fac[n2], M - 2);
+  for (int i = n2; i >= 1; --i) ifac[i - 1] = 1LL * ifac[i] * i % M;
   auto C = [&](int x, int y) -> int {
     if (y < 0 || y > x) return 0;
     return 1LL * fac[x] * ifac[y] % M * ifac[x - y] % M;
   };
-  int cl = 0, cr = 0, ct = 0;
+  std::map<char, int> mp{{'?', 0}, {'B', 1}, {'W', 2}};
+  int wc[3]{}, wwc[3]{}, ww = 0;
+  bool flag = false;
   for (int i = 0; i < n; ++i) {
-    if (l[i] == 0) ++cl;
-    else if (l[i] == 2) ++ct;
-    if (r[i] == 0) ++cr;
-    else if (r[i] == 1) --ct;
+    char cl, cr;
+    std::cin >> cl >> cr;
+    int l = mp[cl];
+    int r = mp[cr];
+    ++wc[l];
+    ++wc[r];
+    ++wwc[l];
+    if (r == 0) {
+      ++wwc[r];
+      if (l == 0) ++ww;
+    } else {
+      if (l == r) flag = true;
+      ++wwc[3 - r];
+    }
   }
-  int ans = 0;
-  for (int i = 0; i <= cl; ++i) {
-    ans = (ans + 1LL * C(cl, i) * C(cr, i + ct)) % M;
+  if (wc[1] > n || wc[2] > n) return 0;
+  int ans = C(n2 - wc[1] - wc[2], n - wc[1]);
+  if (!flag) {
+    ans -= powMod(2, ww);
+    if (ans < 0) ans += M;
+    if (wwc[1] == 0) ++ans;
+    if (wwc[2] == 0) ++ans;
   }
   return ans;
 }
