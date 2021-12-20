637bf56f15912d69b5533583006296c2a84b5ef6
izlyforever <izlyforever@outlook.com>
2021-12-20 16:18:02 +0800

    add nodiscard.cpp

---
+++ example/nodiscard.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+[[nodiscard]] int f(int x) {
+  return x * x;
+}
+
+int main() {
+  //  warning: ignoring return value of 'int f(int)', declared with attribute 'nodiscard' [-Wunused-result]
+  f(3);
+  return 0;
+}
