ec2fbfc59fcbc59fd3e084bac561436a700887cb
izlyforever <izlyforever@outlook.com>
2021-12-29 20:19:15 +0800

    update

--- contest/1615/d/d.cpp
+++ contest/1615/d/d.cpp
@@ -3,7 +3,81 @@
 using LL = long long;
 
 void solve() {
-
+  int n, m;
+  std::cin >> n >> m;
+  std::vector<std::vector<int>> e(n), g(n);
+  std::vector<std::tuple<int, int, int>> edge(n - 1);
+  for (auto &[x, y, v] : edge) {
+    std::cin >> x >> y >> v;
+    --x; --y;
+    if (v != -1) {
+      if (__builtin_parity(v)) {
+        e[x].emplace_back(y);
+        e[y].emplace_back(x);
+      } else {
+        g[x].emplace_back(y);
+        g[y].emplace_back(x);
+      }
+    }
+  }
+  for (int i = 0; i < m; ++i) {
+    int x, y, p;
+    std::cin >> x >> y >> p;
+    --x; --y;
+    if (p) {
+      e[x].emplace_back(y);
+      e[y].emplace_back(x);
+    } else {
+      g[x].emplace_back(y);
+      g[y].emplace_back(x);
+    }
+  }
+  std::vector<int> val(n, -1), vis(n);
+  auto bfs = [&](int root) -> bool {
+    if (vis[root]) return true;
+    std::queue<int> Q;
+    Q.push(root);
+    val[root] = 0;
+    vis[root] = 1;
+    while (!Q.empty()) {
+      int u = Q.front();
+      Q.pop();
+      for (auto v : e[u]) {
+        if (val[v] == -1) {
+          val[v] = 1 - val[u];
+        } else if (val[v] != 1 - val[u]) {
+          return false;
+        }
+        if (!vis[v]) {
+          vis[v] = 1;
+          Q.push(v);
+        }
+      }
+      for (auto v : g[u]) {
+        if (val[v] == -1) {
+          val[v] = val[u];
+        } else if (val[v] != val[u]) {
+          return false;
+        }
+        if (!vis[v]) {
+          vis[v] = 1;
+          Q.push(v);
+        }
+      }
+    }
+    return true;
+  };
+  for (int i = 0; i < n; ++i) {
+    if (!bfs(i)) {
+      std::cout << "NO\n";
+      return;
+    }
+  }
+  std::cout << "YES\n";
+  for (auto &[x, y, v] : edge) {
+    if (v == -1) v = val[x] ^ val[y];
+    std::cout << ++x << ' ' << ++y << ' ' << v << '\n';
+  }
 }
 
 int main() {
--- contest/1615/e/e.cpp
+++ contest/1615/e/e.cpp
@@ -3,13 +3,55 @@
 using LL = long long;
 
 void solve() {
-
+  int k, n;
+  std::cin >> n >> k;
+  std::vector<std::vector<int>> e(n);
+  for (int i = 1; i < n; ++i) {
+    int x, y;
+    std::cin >> x >> y;
+    --x; --y;
+    e[x].emplace_back(y);
+    e[y].emplace_back(x);
+  }
+  std::vector<int> dep(n), fa(n);
+  std::function<void(int)> dfs = [&](int u) {
+    for (auto v : e[u]) if (v != fa[u]) {
+      dep[v] = dep[u] + 1;
+      fa[v] = u;
+      dfs(v);
+    }
+  };
+  dfs(0);
+  std::vector<int> p(n);
+  std::iota(p.begin(), p.end(), 0);
+  std::sort(p.begin(), p.end(), [&dep](int i, int j) {
+    return dep[i] > dep[j];
+  });
+  std::vector<int> vis(n), val(n);
+  for (auto x : p) {
+    val[x] = 0;
+    int cur = x;
+    while (vis[cur] == 0) {
+      ++val[x];
+      vis[cur] = 1;
+      cur = fa[cur];
+    }
+  }
+  std::sort(val.begin(), val.end(), std::greater<>());
+  int left = n;
+  LL ans = INT64_MIN;
+  for (int i = 0; i <= k; ++i) {
+    int b = std::min(n / 2, left);
+    ans = std::max(ans, 1LL * b * (b - n) + 1LL * i * (n - i));
+    if (i < k) left -= val[i];
+  }
+  std::cout << ans << '\n';
 }
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int cas = 1;
-  std::cin >> cas;
+  // std::cin >> cas;
   while (cas--) {
     solve();
   }
