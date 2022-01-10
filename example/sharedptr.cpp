ab65a70c046489dc925f5a0b5c647a66ec4bad80
izlyforever <izlyforever@outlook.com>
2022-01-10 19:41:59 +0800

    add sharedptr

---
+++ example/sharedptr.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define cerr(x) std::cerr << (#x) << " is " << (x) << '\n'
+using LL = long long;
+
+struct Foo {
+    Foo(int n = 0) noexcept : bar(n) {
+        std::cout << "Foo: constructor, bar = " << bar << '\n';
+    }
+    ~Foo() {
+         std::cout << "Foo: destructor, bar = " << bar << '\n';
+    }
+    int getBar() const noexcept { return bar; }
+private:
+    int bar;
+};
+
+void solve() {
+  std::shared_ptr<Foo> sptr = std::make_shared<Foo>(1);
+  sptr.reset();
+  std::shared_ptr<int> a = std::make_shared<int>(13);
+  auto b = a;
+  a.reset(new int(12));
+  if (b) {
+    cerr(*b);
+  }
+  if (a) {
+    cerr(*a);
+  }
+}
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int cas = 1;
+  // std::cin >> cas;
+  while (cas--) {
+    solve();
+  }
+  return 0;
+}
