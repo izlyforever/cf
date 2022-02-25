d9c32e7831f10a6d153b186ff84c1f4cee133d3d
izlyforever <izlyforever@outlook.com>
2022-02-25 15:00:53 +0800

    update

--- contest/1642/d/d.cpp
+++ contest/1642/d/d.cpp
@@ -4,7 +4,52 @@ using LL = long long;
 
 void solve() {
   int n;
-  // std::vector<int>
+  std::cin >> n;
+  std::deque<int> Q;
+  std::map<int, int> mp;
+  for (int i = 0; i < n; ++i) {
+    int x;
+    std::cin >> x;
+    Q.push_back(x);
+    ++mp[x];
+  }
+  for (auto &[_, x] : mp) {
+    if (x & 1) {
+      std::cout << -1 << '\n';
+      return;
+    }
+  }
+  std::vector<std::pair<int, int>> op;
+  std::vector<int> ans{0};
+  int cur = 0;
+  while (!Q.empty()) {
+    int x = Q.front();
+    int tmp = 1;
+    while (Q[tmp] != x) ++tmp;
+    cur += tmp;
+    for (int i = 1; i < tmp; ++i) {
+      op.emplace_back(cur + i, Q[i]);
+    }
+    cur += tmp;
+    ans.emplace_back(cur);
+    std::queue<int> S;
+    Q.pop_front();
+    for (int i = 0; i < tmp - 1; ++i) {
+      S.push(Q.front());
+      Q.pop_front();
+    }
+    Q.pop_front();
+    while (!S.empty()) {
+      Q.push_front(S.front());
+      S.pop();
+    }
+  }
+  std::cout << op.size() << '\n';
+  for (auto [x, y] : op) std::cout << x << ' ' << y << '\n';
+  std::cout << ans.size() - 1 << '\n';
+  for (int i = 1, m = (int)ans.size(); i < m; ++i) {
+    std::cout << ans[i] - ans[i - 1] << '\n';
+  }
 }
 
 int main() {
--- contest/1642/e/e.cpp
+++ contest/1642/e/e.cpp
@@ -3,13 +3,56 @@
 using LL = long long;
 
 void solve() {
-
+  int n, q;
+  std::cin >> n >> q;
+  std::set<int> S;
+  for (int i = 0; i <= n + 1; ++i) S.insert(i);
+  std::map<int, int> mp;
+  for (int i = 0; i < q; ++i) {
+    int op;
+    std::cin >> op;
+    if (op) {
+      int j;
+      std::cin >> j;
+      if (!S.count(j)) {
+        std::cout << "NO\n";
+      } else {
+        int l = *std::prev(S.find(j));
+        int r = *std::next(S.find(j));
+        auto it = mp.upper_bound(l);
+        // there is one element in [it->first, it->second]
+        if (it != mp.end() && it->second < r) {
+            std::cout << "YES\n";
+        } else {
+            std::cout << "N/A\n";
+        }
+      }
+    } else {
+      int l, r, x;
+      std::cin >> l >> r >> x;
+      if (x == 0) {
+        auto it = S.lower_bound(l);
+        while (it != S.end() && *it <= r) {
+          it = S.erase(it);
+        }
+      } else {
+        auto it = mp.lower_bound(l);
+        if (it != mp.end() && it->second <= r) continue;
+        mp[l] = r;
+        it = mp.find(l);
+        while (it != mp.begin() && r <= std::prev(it)->second) {
+          mp.erase(std::prev(it));
+          it = mp.find(l);
+        }
+      }
+    }
+  }
 }
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int cas = 1;
-  std::cin >> cas;
+  // std::cin >> cas;
   while (cas--) {
     solve();
   }
