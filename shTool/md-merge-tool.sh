287dfb4d36119910a73182b778f09d31199abf82
izlyforever <izlyforever@outlook.com>
2022-04-04 15:53:19 +0800

    refine

--- shTool/md-merge-tool.sh
+++ shTool/md-merge-tool.sh
@@ -6,12 +6,15 @@ insertPosition=`cat -n CHANGES.md|grep 'API file name #2'|head -1|awk '{print $1
 index=-3
 cat $3 | while read line
 do
-  echo "${line}" >> $2.back
   index=$((index + 1))
   if [ $index == $insertPosition ]; then
+    if [ -n "$line$" ]; then
+      printf "\n" >> $2.back
+    fi
     # put diff context: $2 - $1 + $3 to $2.back
     diff $1 $2 --ignore-space-change --unchanged-line-format="" --new-line-format="%L" >> $2.back
   fi
+  echo "${line}" >> $2.back
 done
 
 # let $2 be $2.back
