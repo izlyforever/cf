4bf09c1cd5207a7f2151e9c05ee53ed30c6a028e
izlyforever <izlyforever@outlook.com>
2022-05-04 17:09:51 +0800

    refine

--- example/crash.cpp
+++ example/crash.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 
+// use VS  debug -> save dump as crash.dmp
+// https://blog.csdn.net/gdnh22a/article/details/110437968 for detail
+
 int divCrash(int x) {
   int ans = 100 / x;
   return ans;
